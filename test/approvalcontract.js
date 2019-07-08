1def8062a0322a4bac81df8b104783615c80ec7a
Dharti <47648272+D1830p@users.noreply.github.com>
2019-07-07 23:04:25 -0400

    Add files via upload

---
+++ test/approvalcontract.js
@@ -0,0 +1,11 @@
+const ApprovalContract = artifacts.require('../../contracts/ApprovalContract.sol');
+
+    contract('ApprovalContract', function (accounts) {
+        
+    it('initiates contract' , async function(){
+        const contract = await ApprovalContract.deployed();
+        const approver = await contract.approver.call();
+        assert.equal(approver, 0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef , "approvers don't match" );
+});
+    
+    })
